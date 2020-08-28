0c93f1be1135cfc6d42bdb5e2789ae2f8f257ae6
CrystalVulpine <crystalvulpine@gmail.com>
2020-08-27 21:47:13 -0400

    Remove blocked guilds from search results and explore pages

--- ruqqyou.js
+++ ruqqyou.js
@@ -70,6 +70,17 @@ function filterGuilds() {
 				}
 			}
 		}
+		
+		let modals = document.getElementsByClassName('card-title');
+		for (const modal of modals) {
+			let modalname = modal.getAttribute('href').substring(2);
+			for (const bg of bannedGuilds) {
+				if (modalname.toLowerCase() === bg.toLowerCase()) {
+					let elem = modal.parentNode.parentNode.parentNode.parentNode;
+					elem.parentNode.removeChild(elem);
+				}
+			}
+		}
 	});
 }
 
@@ -78,16 +89,17 @@ function setPageBanned(name) {
 	let newHTML = '<div class="row justify-content-around" id="main-content-row"><div class="col h-100 " id="main-content-col"><div class="row justify-content-center"><div class="col-10 col-md-5"><div class="text-center px-3 my-8"><span class="fa-stack fa-5x text-muted mb-5"><i class="fad fa-chess-rook fa-stack-1x"></i><i class="far text-danger fa-ban fa-stack-2x"></i></span><h1 class="h5">+' + name + ' is banned.</h1><p class="text-muted mb-5">Reason: Blocked by Ruqqyou</p><div><a href="/" class="btn btn-primary">Go to frontpage</a></div></div></div></div></div></div>';
 	document.getElementsByClassName('container-fluid')[1].outerHTML = newHTML;
 	// the list is live, so since we removed the previous element we're still at index of 1 for the next one
-	let next = document.getElementsByClassName('container-fluid')[1];
-	if (next) {
-		next.outerHTML = '';
+	let elem = document.getElementsByClassName('container-fluid')[1];
+	if (elem) {
+		elem.parentNode.removeChild(elem);
 	}
 }
 
 function setPageUserBanned(name) {
 	let newHTML = '<div class="row justify-content-around" id="main-content-row"><div class="col h-100 custom-gutters" id="main-content-col"><div class="row no-gutters"><div class="col-12"><div class="text-center py-7 py-md-8"><span class="fa-stack fa-2x text-muted mb-4"><i class="fas fa-square text-danger opacity-25 fa-stack-2x"></i><i class="fas text-danger fa-user-slash fa-stack-1x text-lg"></i></span><h2 class="h5">@' + name + ' is banned</h2><p class="text-muted">This account has been suspended from Ruqqus.</p><p class="text-small text-muted"><span class="font-weight-bold">Reason:</span> Blocked by Ruqqyou</p></div></div></div></div></div>';
 	document.getElementsByClassName('container-fluid')[1].outerHTML = newHTML;
-	document.getElementsByClassName('container-fluid')[1].outerHTML = '';
+	let elem = document.getElementsByClassName('container-fluid')[1];
+	elem.parentNode.removeChild(elem);
 }
 
 function blockPage(url) {
